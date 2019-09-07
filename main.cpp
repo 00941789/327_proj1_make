b21765415019a0b8f5b8b609e4100ec9556a6bf8
= <Jimmy.allahmensah.17@cnu.edu>
2019-09-06 23:50:06 -0400

    Done

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-//#includes are ALWAYS at top of file
 #include <iostream>	//iostream is where std::cout and std::endl are defined
-
-std::string func()
-{
-	return "hello";
-}
+#include <string.h>
+#include "myfunc.h"
 
 int main()
 {
-	std::cout<<func()<<std::endl;
+	std::string a = func();
+	std::cout << a << std::endl;
 	return 0;
 }
